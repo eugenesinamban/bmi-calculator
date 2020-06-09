932c6bc5155b416c73b3c63c6f7180fc2d7fde5c
eugenesinamban <49954579+eugenesinamban@users.noreply.github.com>
2020-06-09 23:55:49 +0900

    add exception

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once './vendor/autoload.php';
+require_once './exception/InitializationFailedException.php';
 
 $loader = new \Twig\Loader\FilesystemLoader('./views');
 $twig = new \Twig\Environment($loader, [
